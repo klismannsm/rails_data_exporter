f1e9082f157723459040ef8babf693ab28864c6d
klismannsm <klismannsm@gmail.com>
2017-05-08 14:56:17 -0300

    Add csv exporter code

--- data_exporter/lib/data_exporter.rb
+++ data_exporter/lib/data_exporter.rb
@@ -1,5 +1,47 @@
-require "data_exporter/version"
+require 'csv'
 
 module DataExporter
-  # Your code goes here...
+  class Exporter
+    def initialize(objects, format)
+      @objects = objects
+      @format = format
+    end
+
+    def export
+      case @format
+        when 'csv'
+          export_to_csv
+      end
+    end
+
+    def export_to_tempfile
+      case @format
+        when 'csv'
+          export_to_csv_tempfile
+      end
+    end
+
+    private
+
+    def export_to_csv
+      CSV.generate(headers: true, col_sep: ';') do |csv|
+        write_data_to_csv(csv)
+      end
+    end
+
+    def export_to_csv_tempfile(filename_prefix)
+      Tempfile.open([filename_prefix, '.csv']) do |csv_tempfile|
+        csv_tempfile << export_to_csv
+      end
+    end
+
+    def write_data_to_csv(csv)
+      if @objects.first.respond_to? :export_header
+        csv << @objects.first.export_header
+      end
+      @objects.each do |object|
+        csv << object.export
+      end
+    end
+  end
 end
